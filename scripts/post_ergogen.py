431f44053c9403ed1c6939eeee1d1de9154bef25
Andrew Rae <ajrae.nv@gmail.com>
2023-09-28 14:55:55 -0400

    Almost there...

--- scripts/post_ergogen.py
+++ scripts/post_ergogen.py
@@ -8,6 +8,9 @@ from pcbnew_helpers import export_dsn, save_pcb, load_pcb
 from pathlib import Path
 
 gnd_name = 'GND'
+power_nc_name = 'power'
+power_nets = ['RAW', 'Braw', 'MCU1_1', 'MCU1_24']
+power_net_tw = 500000
 
 def main():
     parser = argparse.ArgumentParser()
@@ -21,8 +24,14 @@ def main():
     gnd_nc = pcbnew.NETCLASS(gnd_name)
     ncs[gnd_name] = gnd_nc
 
+    power_nc = pcbnew.NETCLASS(power_nc_name)
+    power_nc.SetTrackWidth(power_net_tw)
+    ncs[power_nc_name] = power_nc
+
     nc_assignments = pcb.GetNetClassLabelAssignments()
     nc_assignments[gnd_name] = gnd_name
+    for net in power_nets:
+        nc_assignments[net] = power_nc_name
 
     pcb.SynchronizeNetsAndNetClasses(False)
 
--- shell.nix
+++ shell.nix
@@ -2,11 +2,38 @@
 
 with pkgs;
 
+let
+  # Using local kicad with tests disabled because ngspice is still causing
+  # issues (and debug=true enables assertions which causes issues)
+  custom-kicad = kicad.override {
+    stable = false;
+    with3d = false;
+    srcs = {
+      kicad = fetchFromGitHub {
+        owner = "andrewjrae";
+        repo = "kicad-mirror";
+        rev = "17ad4b73c4f1d7e6cd9916e116693dcc31b09850";
+        sha256 = "bKg5xIPc6ITHvW6/HpJT9eMlrae0ZiPpPpPXfLKneZ8=";
+      };
+    };
+  };
+  custom-python3 = python3.override {
+    packageOverrides = self: super: {
+      kicad = super.toPythonModule(custom-kicad.src);
+    };
+  };
+  custom-python3-with-kicad = custom-python3.withPackages(
+    ps: [ps.kicad]
+  );
+  freerouting = callPackage ./nix/freerouting {};
+in
 mkShell {
+  name = "keeb-utils";
   buildInputs = [
     nodejs
-    kicad-small
-    (python3.withPackages(ps: with ps; [kicad]))
+    custom-kicad
+    custom-python3-with-kicad
+    freerouting
   ];
   shellHook = ''
     npm install
