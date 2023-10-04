f2f05d16933c61a8e162046307221ac273e66985
Andrew Rae <ajrae.nv@gmail.com>
2023-10-04 15:32:38 -0400

    Export drill files with gerbers.

--- scripts/export-gerbers.sh
+++ scripts/export-gerbers.sh
@@ -4,4 +4,5 @@ output=${2:-output/gerbers}
 
 mkdir -p $output
 kicad-cli pcb export gerbers --subtract-soldermask -o $output $board
+kicad-cli pcb export drill -o $output/ $board
 zip $output.zip $output/*
